e7c95250c04573835520b3f6469a635950db8579
fathergasc <quaglioniandrea@gmail.com>
2024-08-14 13:22:31 +0200

    add routes and authguard

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ export class RegisterComponent {
   cancelRegister = output<boolean>();
 
   private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
 
   model: any = {};
 
@@ -23,7 +25,7 @@ export class RegisterComponent {
         this.cancel();
       },
       error: (error) => {
-        console.log(error);
+        this.toastr.error(error.error); 
       },
     });
   }
